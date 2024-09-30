a47bd7077841e1ebc5e16d69ce46c77e944edf9f
Ram Charan Cheppala <157338572+ramcharanw11@users.noreply.github.com>
2024-09-30 22:41:46 +0530

    updated some functionalities

--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,47 +1,123 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity,FlatList } from 'react-native'
-import React from 'react'
-const dummyData = [{
-  id: '01',
-  title: 'wash car'
-}, {
-  id: '01',
-  title: 'wash car noooo'
-}
-]
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import React, { useState } from 'react';
+
 const TodoScreen = () => {
-  const renderTodos = ({item , index})=>{
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+
+  const handleAddTodo = () => {
+    if (todo.trim()) {
+      if (editingId) {
+        setTodoList(todoList.map(item => (item.id === editingId ? { ...item, title: todo } : item)));
+        setEditingId(null);
+      } else {
+        setTodoList([...todoList, { id: Date.now().toString(), title: todo }]);
+      }
+      setTodo('');
+    }
+  };
+
+  const handleEditTodo = (id, currentTitle) => {
+    setEditingId(id);
+    setTodo(currentTitle);
+  };
+
+  const handleDeleteTodo = (id) => {
+    Alert.alert('Delete Todo', 'Are you sure you want to delete this todo?', [
+      { text: 'Cancel', style: 'cancel' },
+      {
+        text: 'Delete',
+        onPress: () => {
+          setTodoList(todoList.filter(item => item.id !== id));
+        },
+      },
+    ]);
+  };
+
+  const renderTodos = ({ item }) => {
     return (
-      <View style={{backgroundColor:'grey',borderRadius:6,
-        paddingVertical:12,
-        marginBottom:12,
-      }}>
-        <Text style={{color:'black',fontSize:20,fontWeight:'800'}}>{item.title}</Text>
+      <View
+        style={{
+          backgroundColor: 'grey',
+          borderRadius: 6,
+          paddingVertical: 12,
+          marginBottom: 12,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          paddingHorizontal: 10,
+        }}
+      >
+        <Text style={{ color: 'black', fontSize: 20, fontWeight: '800' }}>
+          {item.title}
+        </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <TouchableOpacity 
+            style={{
+              backgroundColor: 'blue', // Edit button color
+              borderRadius: 5,
+              padding: 8,
+              marginRight: 5,
+            }}
+            onPress={() => handleEditTodo(item.id, item.title)}
+          >
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={{
+              backgroundColor: 'red', // Delete button color
+              borderRadius: 5,
+              padding: 8,
+            }}
+            onPress={() => handleDeleteTodo(item.id)}
+          >
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Delete</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    )
-  }
+    );
+  };
+
   return (
     <View style={{ marginHorizontal: 16 }}>
-      <TextInput style={{
-        borderWidth: 2,
-        borderColor: "grey",
-        borderRadius: 6,
-        paddingHorizontal: 16,
-        paddingVertical: 10,
-      }} placeholder='Add a task'>
-      </TextInput>
-      <TouchableOpacity style={{
-        backgroundColor: 'black',
-        borderRadius: 6,
-        paddingVertical: 5,
-        marginVertical: 30,
-        alignItems: 'center',
-      }}>
-        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: '20' }} >Add</Text>
+      <TextInput
+        style={{
+          borderWidth: 2,
+          borderColor: 'grey',
+          borderRadius: 6,
+          paddingHorizontal: 16,
+          paddingVertical: 10,
+          marginLeft: 5,
+        }}
+        placeholder="Add a task"
+        value={todo}
+        onChangeText={setTodo}
+      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'black',
+          borderRadius: 6,
+          paddingVertical: 5,
+          marginVertical: 30,
+          alignItems: 'center',
+          marginLeft: 5,
+        }}
+        onPress={handleAddTodo}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>
+          {editingId ? 'Update' : 'Add'}
+        </Text>
       </TouchableOpacity>
-      {/* Render todo list */}
-      <FlatList data={dummyData} renderItem={renderTodos}></FlatList>
+      <FlatList
+        data={todoList}
+        renderItem={renderTodos}
+        keyExtractor={(item) => item.id}
+      />
     </View>
-  )
-}
-export default TodoScreen
-const styles = StyleSheet.create({})
+  );
+};
+
+export default TodoScreen;
+
+const styles = StyleSheet.create({});
